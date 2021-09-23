07ce88f636eec362b0a45cbe45f8bbc85717c6c9
yesenia <millan98yesenia@gmail.com>
2021-09-23 10:22:02 +0200

    Agregamos los tipos primitivos

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author milla
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte varByte = 5;    
+    
+        System.out.println("Rango minimo de Byte "+ Byte.MIN_VALUE);
+        System.out.println("Rango minimo del Byte "+ Byte.MAX_VALUE);
+    }
+    
+}
